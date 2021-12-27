d0c9a2af6dd7879283a3b4b5d9746ba8a495d669
wiktoriakeller <wiktoriakeller552@gmail.com>
2021-12-27 14:38:02 +0100

    Add office model

---
+++ DataAccess/Models/Office.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Office
+    {
+        public int OfficeId { get; set; }
+        public int OfficeNumber { get; set; }
+        public int Facility { get; set; }
+    }
+}
