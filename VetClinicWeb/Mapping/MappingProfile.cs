e1e5994400374ca33cc57ce2e0815c345b6f855f
Dawid Michalak <dawid.michalak0905@gmail.com>
2021-12-27 21:10:10 +0100

    Add automapper profile

---
+++ VetClinicWeb/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DataAccess.Models;
+using VetClinicWeb.Models;
+
+namespace VetClinic.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Employee, EmployeeViewModel>().ReverseMap();
+            CreateMap<Facility, FacilityViewModel>().ReverseMap();
+            CreateMap<Office, OfficeViewModel>().ReverseMap();
+            CreateMap<Position, PositionViewModel>().ReverseMap();
+        }
+    }
+}   
