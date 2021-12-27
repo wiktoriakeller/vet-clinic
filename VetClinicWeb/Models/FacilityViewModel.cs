eaa4c1c4142f52f74c028bfa2199426a12f01538
Dawid Michalak <dawid.michalak0905@gmail.com>
2021-12-27 21:10:45 +0100

    Add view model classes

---
+++ VetClinicWeb/Models/FacilityViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace VetClinicWeb.Models
+{
+    public class FacilityViewModel
+    {
+    }
+}
---
+++ VetClinicWeb/Models/PositionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace VetClinicWeb.Models
+{
+    public class PositionViewModel
+    {
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Minimum salary")]
+        public float SalaryMin { get; set; }
+        [Required]
+        [DisplayName("Maximum salary")]
+        public float SalaryMax { get; set; }
+    }
+}
