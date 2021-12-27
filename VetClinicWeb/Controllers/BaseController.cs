480517d4ada64b71e99b0689926bace1a039668a
Dawid Michalak <dawid.michalak0905@gmail.com>
2021-12-27 21:49:57 +0100

    Add base controller

---
+++ VetClinicWeb/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+
+namespace VetClinic.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected readonly IMapper _mapper;
+
+        public BaseController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+    }
+}
