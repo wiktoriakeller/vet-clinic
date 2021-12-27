8d0949ccc1297dedde6ca385c55f557d342d8409
wiktoriakeller <wiktoriakeller552@gmail.com>
2021-12-27 15:15:53 +0100

    Update employee model

--- DataAccess/Models/Employee.cs
+++ DataAccess/Models/Employee.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Models
 {
-    internal class Employee
+    public class Employee
     {
-    }
+		public int EmployeeId { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public double Salary { get; set; }
+		public double BonusSalary { get; set; }
+		public string Address { get; set; }
+		public string PhoneNumber { get; set; }
+		public string Position { get; set; }
+		public string Facility { get; set; }
+	}
 }
