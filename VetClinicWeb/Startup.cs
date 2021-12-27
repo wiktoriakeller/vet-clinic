72a31edfede01d6cf583cfb8b3e76564ab81282a
wiktoriakeller <wiktoriakeller552@gmail.com>
2021-12-27 19:35:52 +0100

    Add dependencies

--- VetClinicWeb/Startup.cs
+++ VetClinicWeb/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccess.DbAccess;
+using DataAccess.Access;
 
 namespace VetClinicWeb
 {
@@ -20,13 +22,15 @@ namespace VetClinicWeb
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<ISQLDataAccess, SQLDataAccess>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddRazorPages();
+            services.AddControllersWithViews();
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +40,6 @@ namespace VetClinicWeb
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
