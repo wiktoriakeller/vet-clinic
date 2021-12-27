7e567c271f492b75f0bb266135cb63c0f1362140
wiktoriakeller <wiktoriakeller552@gmail.com>
2021-12-27 17:33:51 +0100

    Add data access

---
+++ DataAccess/Access/FacilityDataAccess.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.DbAccess;
+using DataAccess.Models;
+using Dapper;
+
+namespace DataAccess.Access
+{
+    public class FacilityDataAccess
+    {
+        private readonly ISQLDataAccess _db;
+
+        public FacilityDataAccess(ISQLDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<IEnumerable<Facility>> GetFacilities()
+        {
+            string sql = "select * from facilities";
+            return _db.LoadData<Facility>(sql);
+        }
+
+        public async Task<Facility> GetFacility(int facilityId)
+        {
+            string sql = "select * from facilities where facilityId = :Id";
+            DynamicParameters dynamicParameters = new DynamicParameters(new
+            {
+                Id = facilityId
+            });
+
+            var results = await _db.LoadData<Facility, DynamicParameters>(sql, dynamicParameters);
+            return results.First();
+        }
+
+        public Task InsertFacility(Facility facility)
+        {
+            string sql = "insert into facilities(address, phoneNumber) values(:Address, :PhoneNumber)";
+            DynamicParameters dynamicParameters = new DynamicParameters(new
+            {
+                Address = facility.Address,
+                PhoneNumber = facility.PhoneNumber
+            });
+
+            return _db.SaveData(sql, dynamicParameters);
+        }
+
+        public Task UpdateFacility(Facility facility)
+        {
+            string sql = "update facilities set address = :Address, phoneNumber = :PhoneNumber where facilityId = :Id";
+
+            DynamicParameters dynamicParameters = new DynamicParameters(new
+            {
+                Id = facility.FacilityId,
+                Address = facility.Address,
+                PhoneNumber = facility.PhoneNumber
+            }); ;
+
+            return _db.SaveData(sql, dynamicParameters);
+        }
+
+        public Task DeleteFacility(int facilityId)
+        {
+            string sql = "delete from facilities where facilityId = :Id";
+
+            DynamicParameters dynamicParameters = new DynamicParameters(new
+            {
+                Id = facilityId
+            });
+
+            return _db.SaveData(sql, dynamicParameters);
+        }
+    }
+}
---
+++ DataAccess/DbAccess/ISQLDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccess.DbAccess
+{
+    public interface ISQLDataAccess
+    {
+        Task<IEnumerable<T>> LoadData<T>(string sql, string connectionId = "DefaultConnection");
+        Task<IEnumerable<T>> LoadData<T, U>(string sql, U data, string connectionId = "DefaultConnection");
+        Task SaveData<T>(string sql, T data, string connectionId = "DefaultConnection");
+    }
+}
