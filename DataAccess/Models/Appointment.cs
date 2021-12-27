66324c89b9476009749c047e0217e402050c49e1
wiktoriakeller <wiktoriakeller552@gmail.com>
2021-12-27 14:23:50 +0100

    Update appointment

--- DataAccess/Models/Appointment.cs
+++ DataAccess/Models/Appointment.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Models
 {
-    internal class Appointment
+    public class Appointment
     {
+        public int AppointmentId { get; set; }
+        public string AppointmentDate { get; set; }
+        public string Cause { get; set; }
+        public int Office { get; set; }
+        public int Employee { get; set; }
+        public int Patient { get; set; }
     }
 }
