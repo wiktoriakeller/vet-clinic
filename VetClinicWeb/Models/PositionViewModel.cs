77847a59404a6ff27b9d81f479bb32d24418067a
wiktoriakeller <wiktoriakeller552@gmail.com>
2021-12-28 17:18:11 +0100

    Update validation for position model

--- VetClinicWeb/Models/PositionViewModel.cs
+++ VetClinicWeb/Models/PositionViewModel.cs
@@ -5,15 +5,21 @@ namespace VetClinicWeb.Models
 {
     public class PositionViewModel
     {
-
         public int PositionId { get; set; }
+        
         [Required]
+        [DataType(DataType.Text)]
+        [RegularExpression(@"[A-Za-z]*", ErrorMessage = "Position name should only contain letters.")]
         public string Name { get; set; }
+        
         [Required]
         [DisplayName("Minimum salary")]
+        [DataType(DataType.Currency)]
         public float SalaryMin { get; set; }
+        
         [Required]
         [DisplayName("Maximum salary")]
+        [DataType(DataType.Currency)]
         public float SalaryMax { get; set; }
     }
 }
