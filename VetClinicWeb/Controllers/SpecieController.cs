607337911cc47adf94906e5868a5b5f295846f9f
wiktoriakeller <wiktoriakeller552@gmail.com>
2021-12-29 23:47:58 +0100

    Update specie controller

--- VetClinicWeb/Controllers/SpecieController.cs
+++ VetClinicWeb/Controllers/SpecieController.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using DataAccess.Access;
 using DataAccess.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -9,19 +8,15 @@ using VetClinicWeb.Models;
 
 namespace VetClinicWeb.Controllers
 {
-    public class SpecieController : BaseController
+    public class SpecieController : BaseOperationsController<Specie, SpecieViewModel>
     {
-        private readonly IDataAccess<Specie> _specieDataAccess;
-        public SpecieController(IDataAccess<Specie> SpecieDataAccess, IMapper mapper) : base(mapper)
-        {
-            _specieDataAccess = SpecieDataAccess;
-        }
+        public SpecieController(IDataAccess<Specie> specieDataAccess, IMapper mapper) : base(mapper, specieDataAccess) { }
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            IEnumerable<Specie> dbSpecies = await _specieDataAccess.Get();
-            List<SpecieViewModel> species = new List<SpecieViewModel>();
+            IEnumerable<Specie> dbSpecies = await _dataAccess.Get();
+            var species = new List<SpecieViewModel>();
 
             foreach (Specie dbSpecie in dbSpecies)
                 species.Add(_mapper.Map<SpecieViewModel>(dbSpecie));
@@ -29,71 +24,21 @@ namespace VetClinicWeb.Controllers
             return View(species);
         }
 
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create(SpecieViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                await _specieDataAccess.Insert(_mapper.Map<Specie>(model));
-                ModelState.Clear();
-                return RedirectToAction("Index");
-            }
-
-            return View(model);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Update(int id)
-        {
-            Specie specie = await _specieDataAccess.Get(id);
-            return View(_mapper.Map<SpecieViewModel>(specie));
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Update(int id, SpecieViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                await _specieDataAccess.Update(_mapper.Map<Specie>(model));
-                ModelState.Clear();
-                return RedirectToAction("Index");
-            }
-
-            return View(model);
-        }
-
-        [HttpGet]
-        public IActionResult ShowDelete(int id)
-        {
-            return RedirectToAction("Delete", new { id = id });
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Delete(int id)
-        {
-            Specie specie = await _specieDataAccess.Get(id);
-            return View(_mapper.Map<SpecieViewModel>(specie));
-        }
 
         [HttpPost]
         public async Task<IActionResult> Delete(int id, SpecieViewModel model)
         {
             try
             {
-                await _specieDataAccess.Delete(id);
+                await _dataAccess.Delete(id);
             }
             catch (Oracle.ManagedDataAccess.Client.OracleException ex)
             {
                 ViewBag.ErrorMessage = $"Specie {GetExceptionMessage(ex.Number)}";
-                var specie = await _specieDataAccess.Get(id);
+                var specie = await _dataAccess.Get(id);
                 return View(_mapper.Map<SpecieViewModel>(specie));
             }
+
             return RedirectToAction("Index");
         }
     }
