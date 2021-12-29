ded615b140000d0ec6eac6c10c000e7530bd8843
Dawid Michalak <dawid.michalak0905@gmail.com>
2021-12-29 14:52:53 +0100

    Add specie view model

---
+++ VetClinicWeb/Models/SpecieViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VetClinicWeb.Models
+{
+    public class SpecieViewModel
+    {
+        public int SpeciesId { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "{0} length must be between {2} and {1} characters", MinimumLength = 1)]
+        public string Name { get; set; }
+
+    }
+}
