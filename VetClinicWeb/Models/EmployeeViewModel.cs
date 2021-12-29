d872725f8dad332405971ed3172b622cd6579314
Dawid Michalak <dawid.michalak0905@gmail.com>
2021-12-29 11:01:53 +0100

    Add employee view model without validation

--- VetClinicWeb/Models/EmployeeViewModel.cs
+++ VetClinicWeb/Models/EmployeeViewModel.cs
@@ -1,6 +1,26 @@
-﻿namespace VetClinicWeb.Models
+﻿using DataAccess.Models;
+using System.ComponentModel;
+
+namespace VetClinicWeb.Models
 {
     public class EmployeeViewModel
     {
+		[DisplayName("Employee ID")]
+		public int EmployeeId { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public double Salary { get; set; }
+		[DisplayName("Bonus salary")]
+		public double BonusSalary { get; set; }
+		public string Address { get; set; }
+		[DisplayName("Phone number")]
+		public string PhoneNumber { get; set; }
+		public int Position { get; set; }
+		public int Facility { get; set; }
+
+		[DisplayName("Position")]
+		public string PositionName { get; set; }
+		[DisplayName("Facility")]
+		public string FacilityAddress { get; set; }
     }
 }
