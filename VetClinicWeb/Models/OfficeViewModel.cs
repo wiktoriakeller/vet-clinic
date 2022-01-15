a84139f1714c41bcaaa527aae8ab5a61503391f7
Dawid Michalak <dawid.michalak0905@gmail.com>
2022-01-15 18:06:13 +0100

    Add office

--- VetClinicWeb/Models/OfficeViewModel.cs
+++ VetClinicWeb/Models/OfficeViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace VetClinicWeb.Models
+﻿using DataAccess.Models;
+using System.ComponentModel;
+
+namespace VetClinicWeb.Models
 {
     public class OfficeViewModel
     {
+        public int OfficeId { get; set; }
+        [DisplayName("Office number")]
+        public int OfficeNumber { get; set; }
+        public int Facility { get; set; }
+        public string Address{ get; set; }
     }
 }
